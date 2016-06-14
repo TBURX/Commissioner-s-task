af56adcebc5ef2b39b41f5c77cc0ffd7c2227c75
Vitaly <subbot.v@gmail.com>
2016-06-14 20:17:54 +0500

    доставание пюстолета - ок, скрытие после задержки 50/50

--- Assets/GameScripts/GetGun.cs
+++ Assets/GameScripts/GetGun.cs
@@ -1,15 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GetGun : MonoBehaviour {
 
     private bool gunIsHide = true;
     private Animator m_Anim;
-    private Animator m_Anim2;
     public GameObject gun;
+    public GameObject vertebrae;
+    public GameObject head;
     // Use this for initialization
     void Start () {
         m_Anim = GetComponent<Animator>();
-        //m_Anim2 = GameObject.FindGameObjectWithTag("Gun").GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -17,25 +18,62 @@ public class GetGun : MonoBehaviour {
 	
 	}
 
+    IEnumerator OnHold()
+    {
+        m_Anim.SetBool("gun", true);
+        m_Anim.SetFloat("HideGun", 1);
+        yield return new WaitForSeconds(0.5f);
+        gun.transform.parent = vertebrae.transform;
+        gun.transform.localPosition = new Vector3(-4f, -15.7f, 4.3f);
+        gun.transform.localEulerAngles = new Vector3(4.4f, 9.27f, 88.78f);
+        gunIsHide = !gunIsHide;
+    }
+
+    IEnumerator OnShoot()
+    {
+        m_Anim.SetBool("gun", true);
+        m_Anim.SetFloat("HideGun", 0);
+        yield return new WaitForSeconds(0.5f);
+        gun.transform.parent = head.transform;
+        gun.transform.localPosition = new Vector3(-49.18f, -0.46f, -13.14f);
+        gun.transform.localEulerAngles = new Vector3(360.88f, -95.24f, -267.99f);
+        gunIsHide = !gunIsHide;
+    }
+
+    IEnumerator HideIfNotShooting()
+    {
+        yield return new WaitForSeconds(5);
+        if (!gunIsHide)
+        {
+            StartCoroutine(OnHold());
+        }
+    }
+
     void FixedUpdate()
     {
+        Coroutine hideAfterTime = null;
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (gunIsHide)
             {
-                m_Anim.SetBool("gun", true);
-                m_Anim.SetFloat("HideGun", 0);
-               // m_Anim2.SetFloat("HideGun", 0);
-                gunIsHide = !gunIsHide;
+                StartCoroutine(OnShoot());
+                hideAfterTime = StartCoroutine(HideIfNotShooting());
             }
             else
-            {
-                m_Anim.SetBool("gun", true);
-                m_Anim.SetFloat("HideGun", 1);
-                //m_Anim2.SetFloat("HideGun", 1);
-                gunIsHide = !gunIsHide;
+            {                
+                StartCoroutine(OnHold());
+                StopCoroutine(hideAfterTime);
             }
         }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (hideAfterTime != null)
+            {
+                StopCoroutine(hideAfterTime);
+                hideAfterTime = StartCoroutine(HideIfNotShooting());
+            }
+        }
+
 
         //Debug.Log(m_Anim.GetCurrentAnimatorStateInfo(0).length);
     }
